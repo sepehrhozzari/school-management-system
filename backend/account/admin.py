3c3fe40428fe120f5cd51737a05bba9c77785408
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 18:34:55 +0430

    create admin list_filter for User model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -4,6 +4,8 @@ from .models import User, Grade, Major
 
 UserAdmin.list_display += ("is_student", "is_teacher",
                            "grade", "major", "father_name")
+UserAdmin.list_filter += ("is_teacher", "is_studnet",
+                          "data_of_birth", "major", "grade")
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Grade)
