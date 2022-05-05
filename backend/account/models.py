4eaa011df187236ab1adb5d2d79479d05c3807f8
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 17:34:31 +0430

    create Grade model

--- backend/account/models.py
+++ backend/account/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from .validators import national_code_validator
 
 
+class Grade(models.Model):
+    name = models.CharField(max_length=10, verbose_name="نام مقطع")
+
+    class Meta:
+        verbose_name = "مقطع تحصیلی"
+        verbose_name_plural = "مقاطع تحصیلی"
+
+    def __str__(self):
+        return self.name
+
+
 class User(AbstractUser):
     is_student = models.BooleanField(default=False, verbose_name="دانش آموز")
     is_teacher = models.BooleanField(default=False, verbose_name="معلم")
@@ -14,3 +25,5 @@ class User(AbstractUser):
         verbose_name="تاریخ تولد", null=True, blank=True)
     father_name = models.CharField(
         max_length=200, null=True, blank=True, verbose_name="نام پدر")
+    grade = models.ForeIgnKey(Grade, null=True, blank=True, on_delete=models.SET_NULL,
+                              related_name="students", verbose_name="مقطع تحصیلی")
