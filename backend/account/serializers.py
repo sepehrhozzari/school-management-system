1ee91829b670e55601e261b75824b9e1d199d903
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 20:28:19 +0430

    overridi major and grade field

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -3,6 +3,9 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    major = serializers.CharField(source="major.name")
+    grade = serializers.CharField(source="grade.name")
+
     class Meta:
         model = User
         exclude = ("password", "user_permissions", "groups")
