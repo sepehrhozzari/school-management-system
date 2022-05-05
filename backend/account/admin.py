7414ee03b75236ddd11b4f66f4398ba71b772cde
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 18:04:58 +0430

    admin register for Major model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Grade
+from .models import User, Grade, Major
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Grade)
+admin.site.register(Major)
