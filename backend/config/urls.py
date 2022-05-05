c25d5ea4088160025dc5b5799ffc9edd23056c5e
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 19:12:06 +0430

    create UserSerializer

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include("account.urls")),
     path('api-auth/', include('rest_framework.urls')),
+
+
+    path('api/account/', include("account.urls")),
 ]
