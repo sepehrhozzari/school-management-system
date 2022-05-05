c614fe5d87aa7aa3218bfb9c5cf097e0ac2a775d
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 18:04:18 +0430

    admin register for Grade

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Grade
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Grade)
