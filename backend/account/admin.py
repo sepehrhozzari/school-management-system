b26cc94da910c59bde36447f6b39432901957cd7
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 18:33:33 +0430

    create admin list_display for User model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Grade, Major
 
+UserAdmin.list_display += ("is_student", "is_teacher",
+                           "grade", "major", "father_name")
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Grade)
