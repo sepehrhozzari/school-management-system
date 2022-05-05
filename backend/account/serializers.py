b1fd8b0074677eaa38607dded8e7e6338156cc4e
sepehr <sepehrhozzari99@gmail.com>
2022-05-05 23:12:23 +0430

    add create method for writable nested serializers

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Grade, Major
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,3 +9,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ("password", "user_permissions", "groups")
+
+    def create(self, validated_data):
+        # pop grade name and major name from validated_data
+        grade_data = validated_data.pop("grade")
+        major_data = validated_data.pop("major")
+
+        # get grade and major instacne
+        try:
+            major = Major.objects.get(**major_data)
+        except:
+            raise serializers.ValidationError(
+                "رشته ای که مورد نظر آن بوده اید پیدا نشده است")
+        try:
+            grade = Grade.objects.get(**grade_data)
+        except:
+            raise serializers.ValidationError(
+                "مقطع تحصیلی که مورد نظر آن بوده اید پیدا نشده است")
+
+        # create user
+        user = User.objects.create(**validated_data, grade=grade, major=major)
+        return user
