7c5189becc342ad9c025e914e880c6569efd5408
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 17:24:33 +0430

    create ModelAdmin for Exam

--- backend/exam/admin.py
+++ backend/exam/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Exam
 
 
-admin.site.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "start_date", "time")
+
+
+admin.site.register(Exam, ExamAdmin)
