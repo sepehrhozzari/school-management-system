1a70c9aa6bce342064718cf57517a739f0e109be
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 13:14:44 +0430

    create IsSuperUser permission

--- backend/account/permissions.py
+++ backend/account/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsAdminOrStudent(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(request.user and request.user.is_staff or request.user and request.user == obj)
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
--- backend/account/views.py
+++ backend/account/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAdminUser
-from .permissions import IsAdminOrStudent
+from .permissions import IsAdminOrStudent, IsSuperUser
 
 
 class UserViewSet(ModelViewSet):
@@ -11,8 +11,10 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
-        if self.action in ["list", "create"]:
+        if self.action == "list":
             permission_classes = [IsAdminUser, ]
+        elif self.action == "create":
+            permission_classes = [IsSuperUser]
         else:
             permission_classes = [IsAdminOrStudent, ]
         return [permission() for permission in permission_classes]
