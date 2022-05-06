156a7d06361ddd51c457a97d2b2d090fb313f0fb
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 23:35:41 +0430

    add IsAdminOrTeacher permission to BookViewSet

--- backend/book/views.py
+++ backend/book/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializers import BookReadSerializer, BookPostSerializer
+from account.permissions import IsAdminOrTeacher
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
+    permission_classes = [IsAdminOrTeacher, ]
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
