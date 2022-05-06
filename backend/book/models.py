5d8f5c8c99d6d83dbdcdf5417a5cb24ae565df59
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 23:09:16 +0430

    create BookManager

--- backend/book/models.py
+++ backend/book/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from account.models import Grade, Major
 
 
+class BookManager(models.Manager):
+    def book_count(self):
+        return self.count()
+
+
 class Book(models.Model):
     name = models.CharField(max_length=200, verbose_name="عنوان کتاب")
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE,
                               related_name="books", verbose_name="مقطع تحصیلی کتاب")
     major = models.ForeignKey(Major, on_delete=models.CASCADE,
                               related_name="books", verbose_name="مرتبط به رشته تحصیلی")
+
+    objects = BookManager()
 
     def __str__(self):
         return self.name
