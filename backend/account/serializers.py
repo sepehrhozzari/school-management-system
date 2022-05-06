2a4bd1aa72cedb9463f4668fcb9efa982ee15dca
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 21:06:12 +0430

    create UserPostSerializer and UserReadSerializer

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -14,7 +14,7 @@ class MajorSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserReadSerializer(serializers.ModelSerializer):
     major = MajorSerializer()
     grade = GradeSerializer()
 
@@ -22,48 +22,11 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         exclude = ("user_permissions", "groups")
 
-    def create(self, validated_data):
-        # pop grade name and major name from validated_data
-        grade_data = validated_data.pop("grade")
-        major_data = validated_data.pop("major")
 
-        # get grade and major instacne
-        try:
-            major = Major.objects.get(**major_data)
-        except:
-            raise serializers.ValidationError(
-                "رشته ای که مورد نظر آن بوده اید پیدا نشده است")
-        try:
-            grade = Grade.objects.get(**grade_data)
-        except:
-            raise serializers.ValidationError(
-                "مقطع تحصیلی که مورد نظر آن بوده اید پیدا نشده است")
-
-        # create user
-        user = User.objects.create(**validated_data, grade=grade, major=major)
-        return user
-
-    def update(self, instance, validated_data):
-        # get grade name and major name from validated_data
-        major_data = validated_data.get("major", "")
-        grade_data = validated_data.get("grade", "")
-
-        # get grade and major instacne
-        try:
-            major = Major.objects.get(**major_data)
-        except:
-            raise serializers.ValidationError(
-                "رشته ای که مورد نظر آن بوده اید پیدا نشده است")
-        try:
-            grade = Grade.objects.get(**grade_data)
-        except:
-            raise serializers.ValidationError(
-                "مقطع تحصیلی که مورد نظر آن بوده اید پیدا نشده است")
-
-        # update user
-        validated_data["major"] = major
-        validated_data["grade"] = grade
-        return super().update(instance, validated_data)
+class UserPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ("user_permissions", "groups")
 
     def validate_national_code(self, value):
         if value == "":
