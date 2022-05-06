352932f278fef51f7ef3a67e2aebfbb8c852b435
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 20:12:21 +0430

    add OrderingFilter to DEFAULT_FILTER_BACKENDS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -163,6 +163,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ],
 }
 SITE_ID = 1
