f181ab0b6af86e0cc42b0b4515c3919df9575f95
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 17:26:44 +0430

    add ordering to ExamAdmin

--- backend/exam/admin.py
+++ backend/exam/admin.py
@@ -5,6 +5,7 @@ from .models import Exam
 class ExamAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "start_date", "time")
     list_filter = ("start_date",)
+    ordering = ("-start_date",)
 
 
 admin.site.register(Exam, ExamAdmin)
