27c1f1e824995393ffdcb5a696abf4c7176ce9e1
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 22:58:40 +0430

    add grade field to book model

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from account.models import Grade
 
 
 class Book(models.Model):
     name = models.CharField(max_length=200, verbose_name="عنوان کتاب")
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE,
+                              related_name="books", verbose_name="مقطع تحصیلی کتاب")
