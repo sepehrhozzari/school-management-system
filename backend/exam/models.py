4f6c481571822eea72b200506d44619df784ce6b
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 17:19:50 +0430

    add __str__ magic method to Exam model

--- backend/exam/models.py
+++ backend/exam/models.py
@@ -6,3 +6,6 @@ class Exam(models.Model):
     description = models.TextField(verbose_name="توضیحات امتحان")
     start_date = models.DateTimeField(verbose_name="زمان شروع امتحان")
     time = models.IntegerField(verbose_name="مدت زمان(به دقیقه)")
+
+    def __str__(self):
+        return self.title
