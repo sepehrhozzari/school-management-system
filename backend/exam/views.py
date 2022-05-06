4afe05191fc07fb34406ae32e1973ea3d7e9557f
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 19:33:57 +0430

    create ExamViewSet

--- backend/exam/views.py
+++ backend/exam/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Exam
+from .serializers import ExamSerializer
+from .permissions import IsTeacherOrReadOnly
 
-# Create your views here.
+
+class ExamViewSet(ModelViewSet):
+    queryset = Exam.objects.all()
+    serializer_class = ExamSerializer
+    permission_classes = [IsTeacherOrReadOnly, ]
