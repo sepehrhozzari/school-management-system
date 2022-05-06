00ca32cdddb25335716a87567c767f3fc19edd47
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 23:01:18 +0430

    add major field to book model

--- backend/book/models.py
+++ backend/book/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from account.models import Grade
+from account.models import Grade, Major
 
 
 class Book(models.Model):
     name = models.CharField(max_length=200, verbose_name="عنوان کتاب")
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE,
                               related_name="books", verbose_name="مقطع تحصیلی کتاب")
+    major = models.ForeignKey(Major, on_delete=models.CASCADE,
+                              related_name="books", verbose_name="مرتبط به رشته تحصیلی")
