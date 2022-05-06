9fc9532cdf712556eddcec9eb98e4327aa29c7c1
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 17:28:11 +0430

    add search_fields to ExamAdmin

--- backend/exam/admin.py
+++ backend/exam/admin.py
@@ -6,6 +6,7 @@ class ExamAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "start_date", "time")
     list_filter = ("start_date",)
     ordering = ("-start_date",)
+    search_fields = ("title", "description")
 
 
 admin.site.register(Exam, ExamAdmin)
