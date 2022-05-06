6c23ea5db085a9d0d19aa105fe0410fd5c9b9c18
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 17:17:22 +0430

    create Exam model

--- backend/exam/models.py
+++ backend/exam/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان امتحان")
+    description = models.TextField(verbose_name="توضیحات امتحان")
+    start_date = models.DateTimeField(verbose_name="زمان شروع امتحان")
+    time = models.IntegerField(verbose_name="مدت زمان(به دقیقه)")
