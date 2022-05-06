d466bf2eedf13368e901f0181ba64e3b2cee18fe
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 22:41:36 +0430

    create DEFAULT_PAGINATION_CLASS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -165,6 +165,8 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 SITE_ID = 1
 REST_USE_JWT = True
