9bee612386145d4199b64bd5c4d3ebc2659b515a
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 23:21:23 +0430

    add major SerializerMethodField to BookSerializer

--- backend/book/serializers.py
+++ backend/book/serializers.py
@@ -3,6 +3,11 @@ from .models import Book
 
 
 class BookReadSerializer(serializers.ModelSerializer):
+    major = serializers.SerializerMethodField()
+
     class Meta:
         model = Book
         fields = "__all__"
+
+    def get_major(self, obj):
+        return obj.major.name
