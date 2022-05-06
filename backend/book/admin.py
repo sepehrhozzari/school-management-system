7d2d82a2170bad9b520defd8ea7bf07946d142b7
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 23:13:36 +0430

    add search_fields to BookAdmin

--- backend/book/admin.py
+++ backend/book/admin.py
@@ -5,6 +5,7 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("name", "major", "grade")
     list_filter = ("major", "grade")
+    search_fields = ("name",)
 
 
 admin.site.register(Book, BookAdmin)
