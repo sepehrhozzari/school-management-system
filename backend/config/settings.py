caf8f6d135b261e39fb46f2596cbe1c312a46e08
sepehr <sepehrhozzari99@gmail.com>
2022-05-06 20:05:34 +0430

    add search_filter to DEFAULT_FILTER_BACKENDS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -162,6 +162,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
     ],
 }
 SITE_ID = 1
